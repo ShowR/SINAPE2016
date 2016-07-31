3a6c2b56a5b4cd3cbd4fdadf6369747815d617f6
ShowR <showr.brots@gmail.com>
2016-07-31 19:07:42 -0300

    upload

---
+++ Inicio.R
@@ -0,0 +1,73 @@
+################################################################################
+### Criar apresentacao 
+################################################################################
+
+
+## Instalando pacotes
+if (!require(slidify)) {
+  
+  # Instala os pacotes necessarios para trabalho
+  install.packages("needs", dep=T)
+  library(needs)
+  needs(ggvis)
+  needs(devtools)
+  needs(shiny)
+  needs(knitr)
+  needs(knitrBootstrap)
+  needs(knitLatex)
+  needs(knitcitations)
+  needs(rmarkdown)
+  needs(XML)
+  needs(openintro)
+  needs(plotrix)
+  needs(leaflet)
+  needs(shinyRGL)
+  needs(rvg)
+  needs(leaflet)
+  needs(rMaps)
+  needs(rCharts)
+  needs(dygraphs)
+  needs(htmltools)
+  needs(htmlwidgets)
+  needs(metricsgraphics)
+  needs(RColorBrewer)
+  needs(networkD3)
+  needs(DT)
+  needs(threejs)
+  needs(DiagrammeR)
+  needs(metricsgraphics)
+  needs(ggthemes)
+  needs(googleVis)
+  needs(RgoogleMaps)
+  needs(TTR)
+  needs(geosphere)
+  needs(ggmap)
+  needs(jpeg)
+  needs(mapproj)
+  needs(maps)
+  needs(proto)
+  needs(quantmod)
+  needs(rjson)
+  needs(stringi)
+  needs(magrittr)
+  needs(dplyr)
+
+  pkts <- c("ramnathv/slidify", 
+            "ramnathv/slidifyLibraries",
+            "ramnathv/rCharts",
+            "ramnathv/rMaps",
+            "ramnathv/rNotebook",
+            "bwlewis/rthreejs")
+
+  install_github(pkts, force=TRUE)
+  library(slidify)
+}  
+
+# Indica diretorio do projeto
+setwd("j://Projetos//SINAPE2016//")
+
+# Monta apresentacao
+slidify('index.Rmd', )
+
+# Executa apresentacao
+runDeck()
